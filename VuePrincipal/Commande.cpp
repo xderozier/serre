6556118d8c5efb3856eeba35bb161c99073272a9
xderozier <xderozier@btslivh.eu>
2019-01-17 17:15:26 +0100

    creation class comande et serre

---
+++ VuePrincipal/Commande.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Commande.cpp
+ * Author: snir2g1
+ * 
+ * Created on 17 janvier 2019, 17:10
+ */
+
+#include "Commande.h"
+
+Commande::Commande() {
+}
+
+
+Commande::~Commande() {
+}
+
---
+++ VuePrincipal/Serre.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Serre.cpp
+ * Author: snir2g1
+ * 
+ * Created on 17 janvier 2019, 17:01
+ */
+
+#include "Serre.h"
+
+Serre::Serre() {
+    infosClimat = new InfosClimat();
+}
+
+Serre::~Serre() {
+}
+
+void Serre::executerCommande(Commande commande){
+    
+}
