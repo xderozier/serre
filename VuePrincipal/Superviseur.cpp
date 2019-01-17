8de45ebba0043186bace3f473f68c79288dbbcf8
ffabre <ffabre@btslivh.eu>
2019-01-17 18:21:39 +0100

    ajout de la classe Superviseur et CLientMeteo

---
+++ VuePrincipal/Superviseur.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Superviseur.cpp
+ * Author: snir2g1
+ * 
+ * Created on 17 janvier 2019, 17:51
+ */
+
+#include "Superviseur.h"
+
+Superviseur::Superviseur() {
+    serre[]=new serre ();
+}
+
+Superviseur::Superviseur(const Superviseur& orig) {
+}
+
+Superviseur::~Superviseur() {
+}
+ClientMeteo* Superviseur::getLeClientMeteo() {
+    
+}
+Superviseur::setVuePrincipale(VuePrincipale uneVuePrincipale){
+    
+}
