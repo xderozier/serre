e08d14bf939f7eb4bd5846dfc7a1103a0f165e84
ocommeng <olivier-alain.commenge@ac-toulouse.fr>
2019-01-29 17:22:54 +0100

    Réparation de certaines classes métier

---
+++ ClasseMetier/Superviseur.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Superviseur.h
+ * Author: snir2g1
+ *
+ * Created on 29 janvier 2019, 16:34
+ */
+
+#ifndef SUPERVISEUR_H
+#define SUPERVISEUR_H
+
+class Superviseur {
+public:
+    Superviseur();
+    Superviseur(const Superviseur& orig);
+    virtual ~Superviseur();
+private:
+
+};
+
+#endif /* SUPERVISEUR_H */
