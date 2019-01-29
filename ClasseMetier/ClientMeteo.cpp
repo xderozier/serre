af0935eebb5072b6d741388fb714b6d0cf32f4a9
xderozier <xderozier@btslivh.eu>
2019-01-29 17:10:22 +0100

    reparartion classe metier

---
+++ ClasseMetier/ClientMeteo.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ClientMeteo.cpp
+ * Author: snir2g1
+ * 
+ * Created on 29 janvier 2019, 16:57
+ */
+
+#include "ClientMeteo.h"
+
+ClientMeteo::ClientMeteo() {
+}
+
+ClientMeteo::ClientMeteo(const ClientMeteo& orig) {
+}
+
+ClientMeteo::~ClientMeteo() {
+}
+
---
+++ ClasseMetier/ClientMeteo.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ClientMeteo.h
+ * Author: snir2g1
+ *
+ * Created on 29 janvier 2019, 16:57
+ */
+
+#ifndef CLIENTMETEO_H
+#define CLIENTMETEO_H
+
+class ClientMeteo {
+public:
+    ClientMeteo();
+    ClientMeteo(const ClientMeteo& orig);
+    virtual ~ClientMeteo();
+private:
+
+};
+
+#endif /* CLIENTMETEO_H */
+
